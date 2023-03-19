364955f9e5114d16074a810d65137d74e0248e52
Martin <itsjutmartin>
2023-03-19 11:57:33 +0300

    step 2 and adding personal avatar

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+import avatarImage from "../images/avataricon.png"; // Import the avatar image
+const greeting = "Hello, I am Martin Yacoub!";
+const bio1 = " 💻 A software developer";
+const bio2 = "👀 I am always looking to learn new technologies and keep up with industry trends.";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,6 +16,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <Avatar size="xl" src={avatarImage}/>
+    <VStack spacing={2} mt={4}>
+      <Heading size="lg" color="white">{greeting}</Heading>
+      <Heading size="md" color="white">{bio1}</Heading>
+      <Heading size="md" color="white">{bio2}</Heading>
+    </VStack>
 
   </FullScreenSection>
 );
