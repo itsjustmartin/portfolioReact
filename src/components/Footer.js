0c368b8e5ba02a1dcd655c1c2f7fd95f63b12df4
Martin <itsjutmartin>
2023-03-22 09:34:40 +0200

    add scroll up button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { Box, Flex } from "@chakra-ui/react";
 
+import { Button } from "@chakra-ui/react";
+import { FaArrowUp } from "react-icons/fa";
+import { useState , useEffect} from "react";
+
 const Footer = () => {
   const today = new Date();
   const year = today.getFullYear();
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollDistance = window.pageYOffset;
+      if (scrollDistance > 1000) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
   return (
     <Box backgroundColor="#18181b">
       <footer>
@@ -16,7 +36,25 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Martin • © {year}</p>
+          <p>Build with React By Martin • © {year}</p>
+          {showButton && (
+  <Button
+    onClick={() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }}
+    position="fixed"
+    bottom="20px"
+    right="20px"
+    borderRadius="50%"
+    bg="blue.500"
+    color="white"
+    size="md"
+    _hover={{ bg: "blue.600" }}
+  >
+    <FaArrowUp />
+  </Button>
+)}
+
         </Flex>
       </footer>
     </Box>
